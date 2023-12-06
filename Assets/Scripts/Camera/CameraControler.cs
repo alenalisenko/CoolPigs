f640999861f7ae93eb1463b031ef2fc1570da5c3
SEMENLISENKO <semfox2008@gmail.com>
2023-12-06 19:07:11 +1000

    Recommit

--- Assets/Scripts/Camera/CameraControler.cs
+++ Assets/Scripts/Camera/CameraControler.cs
@@ -16,8 +16,9 @@ public class CameraControler : MonoBehaviour
         float distance = Vector3.Distance(_transform.position, drons[dronselected].transform.position);
         if (distance != 0)
         {
-            Vector3 direction = Vector3.Normalize(drons[dronselected].transform.position - _transform.position);
-            _transform.position += direction * speedmultiplier * Time.deltaTime;
+            float axisy = -(_transform.position.y - drons[dronselected].transform.position.y) * speedmultiplier;
+            float axisx = -(_transform.position.x - drons[dronselected].transform.position.x) * speedmultiplier;
+            _transform.position += axisy * _transform.TransformDirection(Vector3.up) * Time.deltaTime + axisx * _transform.TransformDirection(Vector3.right) * Time.deltaTime;
         }
     }
 }
