0eb4b34fe5d3260f6a57a97b38d5d4db317f5c04
SEMENLISENKO <semfox2008@gmail.com>
2023-12-09 11:27:22 +1000

    commit

---
+++ Assets/Scripts/Actions/Actions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using UnityEngine;
+public class Actions : MonoBehaviour
+{
+    /*
+   public IEnumerator CellsMove(GameObject drone,Vector3 movedirection, LayerMask obstaclemask, int cellscount)
+    {
+        Transform _transform = drone.GetComponent<Transform>();
+        RaycastHit hit;
+        Ray ray = new Ray(_transform.position, movedirection);
+        bool freeinfront = Physics.Raycast(ray,out hit, 1, obstaclemask);
+        for(int i = 0; i < cellscount; i++)
+        {
+
+        }
+    }
+    */
+}
